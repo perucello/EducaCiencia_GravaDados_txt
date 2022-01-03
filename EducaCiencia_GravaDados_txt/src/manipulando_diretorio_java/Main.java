dcb6319c6c11ccfef5efb489e5a48eb883281ed4
perucello <perucello@hotmail.com>
2022-01-03 20:54:50 -0300

    Java_EducaCiencia_GravaDados_txt

---
+++ EducaCiencia_GravaDados_txt/src/manipulando_diretorio_java/Main.java
@@ -0,0 +1,20 @@
+package manipulando_diretorio_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Endereço - path
+        String pasta = "C:\\EducaCienciaFastCode\\";
+        //Nome do arquivo
+        String nomeArquivo = "Manipulando_diretorio_java";
+        //para Write
+        String nome = "EducaCiencia - ";
+        String email = "educaciencia_fastcode@outlook.com.br";
+
+        Gravar_dados_txt gravaTXTAppend = new Gravar_dados_txt();
+        gravaTXTAppend.gravar(pasta, nomeArquivo, nome, email);
+
+      
+    }
+}
